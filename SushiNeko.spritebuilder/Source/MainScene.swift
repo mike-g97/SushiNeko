093b93f3c6cf28296fdaf7364582081c1d40051f
Mikhael E Gonzalez <mikhael.gonzalez@gmail.com>
2015-07-03 16:00:20 -0400

    Finished adding title screen.

--- SushiNeko.spritebuilder/Source/MainScene.swift
+++ SushiNeko.spritebuilder/Source/MainScene.swift
@@ -4,16 +4,21 @@ enum Side {
     case Left, Right, None
 }
 
+enum GameState {
+    case Title, Ready, Playing, GameOver
+}
+
 class MainScene: CCNode {
     weak var piecesNode :CCNode!
     weak var character: Character!
     var pieces : [Piece] = []
     var pieceLastSide: Side = .Left
     var pieceIndex : Int = 0
-    var gameOver = false
+    var gameState: GameState = .Title
     weak var restartButton: CCButton!
     weak var lifeBar: CCSprite!
     weak var scoreLabel: CCLabelTTF!
+    weak var tapButtons: CCNode!
 
     var score: Int = 0 {
         didSet{
@@ -47,7 +52,8 @@ class MainScene: CCNode {
         var xTouch = touch.locationInWorld().x
         var side = CCDirector.sharedDirector().viewSize().width / 2
         
-        if gameOver { return }
+        if gameState == .GameOver || gameState == .Title { return }
+        if gameState == .Ready { start() }
         
         if xTouch < side {
             character.left()
@@ -83,7 +89,7 @@ class MainScene: CCNode {
     }
     
     func triggerGameOver() {
-        gameOver = true
+        gameState = .GameOver
         restartButton.visible = true
     }
     
@@ -92,19 +98,42 @@ class MainScene: CCNode {
         
         if newPiece.side == character.side { triggerGameOver() }
         
-        return gameOver
+         return gameState == .GameOver
     }
     
     func restart() {
-        var scene = CCBReader.loadAsScene("MainScene")
-        CCDirector.sharedDirector().presentScene(scene)
+        var mainScene = CCBReader.load("MainScene") as! MainScene
+        mainScene.ready()
+        
+        var scene = CCScene()
+        scene.addChild(mainScene)
+        
+        var transition = CCTransition(fadeWithDuration: 0.3)
+        
+        CCDirector.sharedDirector().presentScene(scene, withTransition: transition)
     }
     
     override func update(delta: CCTime) {
-        if gameOver { return }
+        if gameState != .Playing { return }
         timeLeft -= Float(delta)
         if timeLeft == 0 {
             triggerGameOver()
         }
+    }
+    
+    func ready() {
+        gameState = .Ready
+        
+        self.animationManager.runAnimationsForSequenceNamed("Ready")
+        
+        tapButtons.cascadeOpacityEnabled = true
+        tapButtons.opacity = 0.0
+        tapButtons.runAction(CCActionFadeIn(duration: 0.2))
+    }
+    
+    func start() {
+        gameState = .Playing
+        
+        tapButtons.runAction(CCActionFadeOut(duration: 0.2))
     }
 }
